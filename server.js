9e9fef9640ec2e2d6ebe518c8f272b840427d13e
KaeloPlay <kaelo.swm@gmail.com>
2026-03-17 14:22:41 +0700

    first commit (clean)

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
+
+let pr = 'Belum ada pengumuman hari ini.';
+let note = 'Tidak ada catatan tambahan hari ini.';
+
+app.get('/api/pengumuman', (req, res) => {
+    res.status(200).json({
+        pr: pr, note: note
+    });
+});
+
+app.post('/api/pengumuman', (req, res) => {
+    pr = req.body.pr;
+    note = req.body.note;
+    
+    res.status(201).json({
+        message: 'Pengumuman berhasil diperbarui.',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server berjalan pada port ${port}!`);
+});
